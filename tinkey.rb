2cf880d11b0c9516ea808c56f49a0a7c797c4832
Thai Duong <thaidn@gmail.com>
2020-07-13 00:34:11 -0700

    Let Homebrew guess the version from the URLs.

--- tinkey.rb
+++ tinkey.rb
@@ -1,12 +1,11 @@
 class Tinkey < Formula
   desc "A command line tool to generate and manipulate keysets for the Tink cryptography library"
   homepage "https://github.com/google/tink/tree/master/tools/tinkey"
-  url "https://storage.googleapis.com/tinkey/tinkey-darwin-x86_64.tar.gz"
+  url "https://storage.googleapis.com/tinkey/tinkey-darwin-x86_64-1.4.0.tar.gz"
   sha256 "3da539f6b51b95f7856ff84624d3771809709bf5019d3921343152c1ef7b0eb2"
-  version "1.4.0"
 
   on_linux do
-    url "https://storage.googleapis.com/tinkey/tinkey-linux-x86_64.tar.gz"
+    url "https://storage.googleapis.com/tinkey/tinkey-linux-x86_64-1.4.0.tar.gz"
     sha256 "93faf7c672fac45f210c5bdfe62839f7b8062d330e3326cfcd761c90a24a4939"
   end
 
